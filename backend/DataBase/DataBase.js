c8235149181b74dd4115290d97cfc889f70a5cf2
vanshtiwari211797798 <vanshtiwari9091@gmail.com>
2024-10-28 11:01:08 +0530

    backend changes 28/10/2024

--- backend/DataBase/DataBase.js
+++ backend/DataBase/DataBase.js
@@ -3,16 +3,16 @@ const mongoose = require('mongoose');
 // const URL = 'mongodb://localhost:27017/HOTELBOOKING';
 
 // MongoDb Cloud Atlas Url
-const URL = 'mongodb+srv://tiwarivansh372:Tiwari211797798@cluster0.5g7jn.mongodb.net/HotelBooking?retryWrites=true&w=majority&appName=Cluster0'
+const URL = 'mongodb+srv://tiwarivansh372:Tiwari211797798@cluster0.5g7jn.mongodb.net/HotelBooking?retryWrites=true&w=majority&appName=Cluster0';
 
 const ConnectDB = async () => {
-    try {
-      await mongoose.connect(URL);
-      console.log('DataBase connected successfully')  
-    } catch (error) {
-       console.error('Unable to Connect DataBase');
-       process.exit(0); 
-    }
+  try {
+    await mongoose.connect(URL);
+    console.log('DataBase connected successfully')
+  } catch (error) {
+    console.error('Unable to Connect DataBase');
+    process.exit(0);
+  }
 }
 
 module.exports = ConnectDB;
