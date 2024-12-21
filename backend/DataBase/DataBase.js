709e794d0da96c8d836148f9493d8b92552be9c4
vanshtiwari211797798 <vanshtiwari9091@gmail.com>
2024-12-21 14:20:37 +0530

    db changes

--- backend/DataBase/DataBase.js
+++ backend/DataBase/DataBase.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 // My local system DataBase Url MongoDB Server
-const URL = 'mongodb://localhost:27017/HOTELBOOKING';
+// const URL = 'mongodb://localhost:27017/HOTELBOOKING';
 
 // MongoDb Cloud Atlas Url MongoDb Atlab cloud 
-// const URL = 'mongodb+srv://tiwarivansh372:Tiwari211797798@cluster0.5g7jn.mongodb.net/HotelBooking?retryWrites=true&w=majority&appName=Cluster0';
+const URL = 'mongodb+srv://tiwarivansh372:Tiwari211797798@cluster0.5g7jn.mongodb.net/HotelBooking?retryWrites=true&w=majority&appName=Cluster0';
 
 
 const ConnectDB = async () => {
